f6683c7717593b725245c55e89697b76da69a441
ljyh223 <3439426154@qq.com>
2025-12-14 18:26:06 +0800

    底部tab添加了关注列表界面

--- lib/screens/main_tab_screen.dart
+++ lib/screens/main_tab_screen.dart
@@ -7,6 +7,7 @@ import '../presentation/providers/language_provider.dart';
 import '../screens/shops_screen.dart';
 import '../screens/recommendations_screen.dart';
 import '../screens/user_profile_screen.dart';
+import '../screens/following_list_screen.dart';
 
 /// 主页面 - 底部Tab导航
 class MainTabScreen extends StatefulWidget {
@@ -16,18 +17,25 @@ class MainTabScreen extends StatefulWidget {
   State<MainTabScreen> createState() => _MainTabScreenState();
 }
 
-class _MainTabScreenState extends State<MainTabScreen> with SingleTickerProviderStateMixin {
+class _MainTabScreenState extends State<MainTabScreen>
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 3, vsync: this);
-    
+    _tabController = TabController(length: 4, vsync: this);
+
     // 初始化数据
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      Provider.of<RestaurantProvider>(context, listen: false).fetchRestaurants();
-      Provider.of<RecommendationProvider>(context, listen: false).getHomeRecommendations();
+      Provider.of<RestaurantProvider>(
+        context,
+        listen: false,
+      ).fetchRestaurants();
+      Provider.of<RecommendationProvider>(
+        context,
+        listen: false,
+      ).getHomeRecommendations();
     });
   }
 
@@ -50,6 +58,8 @@ class _MainTabScreenState extends State<MainTabScreen> with SingleTickerProvider
               const ShopsScreen(),
               // 推荐发现内容
               const RecommendationsScreen(),
+              // 关注列表内容
+              const FollowingListScreen(),
               // 个人中心内容
               const UserProfileScreen(),
             ],
@@ -58,20 +68,19 @@ class _MainTabScreenState extends State<MainTabScreen> with SingleTickerProvider
             controller: _tabController,
             tabs: [
               // 首页Tab - 餐厅列表
-              Tab(
-                icon: const Icon(Icons.store),
-                text: t.app.home,
-              ),
+              Tab(icon: const Icon(Icons.store), text: t.app.home),
               // 发现Tab
               Tab(
                 icon: const Icon(Icons.people_alt_outlined),
                 text: t.app.recommendations,
               ),
-              // 个人中心Tab
+              // 关注列表Tab
               Tab(
-                icon: const Icon(Icons.person),
-                text: t.app.userProfile,
+                icon: const Icon(Icons.favorite_outline),
+                text: t.app.following,
               ),
+              // 个人中心Tab
+              Tab(icon: const Icon(Icons.person), text: t.app.userProfile),
             ],
           ),
         );
